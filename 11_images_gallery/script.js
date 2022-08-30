e0df647c5618d69dee823eef991ea6364cc8efa7
pontevirguladev <pontovirguladev@gmail.com>
2022-08-30 06:21:45 -0300

    11 - Images Gallery

---
+++ 11_images_gallery/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
